2cf69bd2df59f1b34880f32ee54b56a8861dfd84
Jangey <jangey.lu@gmail.com>
2018-09-13 14:59:33 -0700

    Update Auto Layout of Restaurant Cell

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -37,8 +37,15 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 10
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
